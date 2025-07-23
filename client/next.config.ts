f4c01be62bcdb5ae7aa31b6c86e7f0da911c0856
Harsh Kumar <hemenderkumar3000@gmail.com>
2025-07-23 16:22:57 +0530

    modified nextconfig

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {protocol: "https",
-        hostname: "pm--s3--images0.s3.ap-south-1.amazonaws.com/public",
+        hostname: "pm--s3--images0.s3.ap-south-1.amazonaws.com",
         port: "",
         pathname:"/**"
       }
